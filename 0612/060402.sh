8dd67e643661a17a4b03bc46a3a4633bff3d338a
s19012 <s19012@std.it-college.ac.jp>
2019-06-13 09:45:49 +0900

    0612課題

---
+++ 0612/060402.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage()
+{
+    local script_name=$(basename "$0")
+
+    cat << END
+Usage: $script_name PATTEN [PATH] [NAME_PATTEN]
+Find file in current directory recursively, and print lines which match PATTEN.
+
+    PATH         find file in PATH directory, instead of current directory
+    NAME_PATTEN  specify name patten to find file
+
+Examples:
+    $script_name return
+    $script_name return ~ '*.txt'
+END
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+for file in "$@"
+do
+    if [ -f "$file" ]; then
+        # doでファイル容量を求める
+        du "$file"
+    else
+        # エラーメッセージ
+        echo "${file}: 通常のファイルではありません"
+    fi
+done
---
+++ 0612/060404.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+usage()
+{
+    local script_name=$(basename "$0")
+    cat << END
+Usage: $script_name PATTEN [PATH] [NAME_PATH]
+Find file in current directory recursively, and print lines which match PATTEN.
+
+    PATH            find file in PATH derectory, instead of current directory
+    NAME_PATTEN     specify name patten to find file
+
+Examples:
+    $script_name return
+    $script_name return ~ '*.txt'
+END
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+if [ -d "$1" ]; then
+    # ディレクトリ内のファイルが実行可能ファイルかチェックして
+    # 実行可能ファイルであれば表示
+    for file in $(ls "$1")
+    do
+        # fileが実行ファイルであれば表示
+        if [ ! -r "${1}/${file}" ]; then
+            echo "$file"
+        fi
+    done
+else
+    # エラーメッセージ
+    echo "${1}: ディレクトリではありません"
+fi
