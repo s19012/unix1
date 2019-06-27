03b3c2e1e52a9f18e612d2701e5b11ffa6bd0c93
s19012 <s19012@std.it-college.ac.jp>
2019-06-27 13:30:40 +0900

    0627 test

--- 0627/062703.sh
+++ 0627/062703.sh
@@ -1,5 +1,30 @@
-file=`find ~ -type f 2> /dev/null | wc -l`
+file=`find ${1} -type f 2> /dev/null | wc -l`
 echo "file => ${file}"
 
-dir=`find ~ -type d 2> /dev/null | wc -l`
+dir=`find ${1} -type d 2> /dev/null | wc -l`
 echo "directory => ${dir}"
+
+usage()
+{
+    local script_name=$(basename "$0")
+
+    cat << END
+    使い方: $script_name DIRPATH
+    DIRPATHで指定したディレクトリ下のファイル数/ディレクトリ数を表示
+    DIRPATH - ファイル数/ディレクトリ数を表示するディレクトリパスを指定
+
+END
+}
+
+if [ -d "$1"]; then
+    for file in $(ls "$1")
+    do
+        if [ -x "${1}/${file}" ]; then
+            echo "$file"
+        fi
+    done
+else
+    echo "${1}: ディレクトリではありません"
+fi
+
+
